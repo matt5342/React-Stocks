16698ea03e65dd76715b3dc0de37b1ce85d8d9e8
Matt <mattfsewell@gmail.com>
2021-03-21 16:05:56 -0400

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,91 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [], 
+    filter: 'all', 
+    sort: 'none', 
+    myIds: []
+  }
+  componentDidMount(){
+    this.fetchStocks()
+  }
+  fetchStocks = () => {
+    fetch('http://localhost:3000/stocks')
+    .then(r => r.json())
+    .then(stockData => this.setState({stocks: stockData}))
+  }
+
+  renderStocks = () => {
+    if (this.state.filter === 'all'){
+      return <StockContainer updatePortfolio={this.updatePortfolio} stocks={this.state.stocks} />
+    }
+    else {
+      let stocksList = this.state.stocks.filter(stock => stock.type === this.state.filter) 
+      if (this.state.sort === 'none'){
+        return <StockContainer updatePortfolio={this.updatePortfolio} stocks={stocksList} />
+      }
+      else if (this.state.sort === 'Alphabetically'){
+        stocksList = stocksList.sort(function (a, b) {
+          if (a.name < b.name) {
+            return -1;
+          }
+          else {return 1;}
+        })
+        return <StockContainer updatePortfolio={this.updatePortfolio} stocks={stocksList} />
+      }
+      else if (this.state.sort === 'Price'){
+        stocksList = stocksList.sort(function (a, b) {
+          if (a.price < b.price) {
+            return -1;
+          }
+          else {return 1;}
+        })
+        return <StockContainer updatePortfolio={this.updatePortfolio} stocks={stocksList} />
+      }
+    }
+  }
+
+
+  updateFilter = e => {
+    let newType = e.target.value
+    this.setState({
+      filter: newType
+    })
+  }
+
+  updateSort = e => {
+    let newSort = e.target.value
+    this.setState({
+      sort: newSort
+    })
+  }
+  updatePortfolio = id => {
+    if (!this.state.myIds.includes(id)) {
+      let newIds = this.state.myIds
+      newIds.push(id)
+      this.setState({
+        myIds: newIds
+      })
+    }
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar updateFilter={this.updateFilter} updateSort={this.updateSort} />
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              {
+                this.renderStocks()
+              /* <StockContainer stocks={this.state.stocks}/> */}
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer myIds={this.state.myIds} stocks={this.state.stocks} />
 
             </div>
           </div>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,11 +3,17 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  renderPortfolio = () => {
+    let myStocks = this.props.stocks.filter(({ id }) => this.props.myIds.includes(id))
+    return myStocks.map(stock => <Stock stock={stock} />)
+
+  }
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
+            this.renderPortfolio()
             //render your portfolio stocks here
           }
       </div>
